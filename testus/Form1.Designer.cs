7dcbccb705ec4666f16a69048e711cee66199d04
stephanus2137 <stefan.owski@icloud.com>
2023-03-29 08:59:14 +0200

    testus

---
+++ testus/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace testus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AAAA = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.szyfruj = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zakonczToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AAAA
+            // 
+            this.AAAA.AutoSize = true;
+            this.AAAA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.AAAA.Location = new System.Drawing.Point(22, 32);
+            this.AAAA.Name = "AAAA";
+            this.AAAA.Size = new System.Drawing.Size(223, 25);
+            this.AAAA.TabIndex = 0;
+            this.AAAA.Text = "Tekst do zaszyfrowania:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(251, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(38, 95);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(29, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(136, 95);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(45, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "a - z";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(227, 95);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(48, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "A - Z";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(318, 95);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(46, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.Text = "0 - 9";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // szyfruj
+            // 
+            this.szyfruj.Location = new System.Drawing.Point(412, 92);
+            this.szyfruj.Name = "szyfruj";
+            this.szyfruj.Size = new System.Drawing.Size(75, 23);
+            this.szyfruj.TabIndex = 6;
+            this.szyfruj.Text = "SZYFRUJ";
+            this.szyfruj.UseVisualStyleBackColor = true;
+            this.szyfruj.Click += new System.EventHandler(this.szyfruj_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "SZYFRUJ",
+            "ODSZYFRUJ"});
+            this.comboBox1.Location = new System.Drawing.Point(208, 136);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "SZYFRUJ";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(521, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zakonczToolStripMenuItem,
+            this.nowyToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "plik";
+            // 
+            // zakonczToolStripMenuItem
+            // 
+            this.zakonczToolStripMenuItem.Name = "zakonczToolStripMenuItem";
+            this.zakonczToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.zakonczToolStripMenuItem.Text = "zakoncz";
+            this.zakonczToolStripMenuItem.Click += new System.EventHandler(this.zakonczToolStripMenuItem_Click);
+            // 
+            // nowyToolStripMenuItem
+            // 
+            this.nowyToolStripMenuItem.Name = "nowyToolStripMenuItem";
+            this.nowyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nowyToolStripMenuItem.Text = "nowy";
+            this.nowyToolStripMenuItem.Click += new System.EventHandler(this.nowyToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 175);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.szyfruj);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.AAAA);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "szyfr cezara";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AAAA;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button szyfruj;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zakonczToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nowyToolStripMenuItem;
+    }
+}
+
---
+++ testus/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testus
+{
+    public partial class Form1 : Form
+    {
+        int ch, kodus = 1;
+        string tekstus, tekstus2;
+        char lit;
+        bool blad = false;
+        bool debil = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            tekstus = textBox1.Text;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            ch = 0;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            ch = 1;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            ch = 2;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                debil = false;
+            }
+            else
+            {
+                debil = true;
+            }
+        }
+
+        private void zakonczToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void nowyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            kodus = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void szyfruj_Click(object sender, EventArgs e)
+        {
+            blad = false;
+            if (debil == false)
+            {
+                for (int i = 0; i < tekstus.Length; i++)
+                {
+                    lit = tekstus[i];
+                    if ((ch == 0) && ((Char.IsUpper(lit)) || (Char.IsDigit(lit))))
+                    {
+                        MessageBox.Show("poza zakresem");
+                        blad = true;
+                        break;
+                    }
+                    else if ((ch == 1) && ((Char.IsLower(lit)) || (Char.IsDigit(lit))))
+                    {
+                        MessageBox.Show("poza zakresem");
+                        blad = true;
+                        break;
+                    }
+                    else if ((ch == 2) && (Char.IsLetter(lit)))
+                    {
+                        MessageBox.Show("poza zakresem");
+                        blad = true;
+                        break;
+                    }
+                    lit = (Char)(Convert.ToUInt16(lit) + kodus);
+                    if ((ch == 2) && (lit > 57))
+                    {
+                        lit = (Char)(Convert.ToUInt16(lit) - 10);
+                    }
+                    else if ((ch == 2) && (lit < 48))
+                    {
+                        lit = (Char)(Convert.ToUInt16(lit) + 10);
+                    }
+                    if ((ch == 0) && (kodus < 0))
+                    {
+                        lit = (Char)(Convert.ToUInt16(lit) + 26);
+                    }
+                    tekstus2 = tekstus2 + lit;
+                }
+            }
+            else if (debil == true)
+            {
+                for (int i = 0; i < tekstus.Length; i++)
+                {
+                    lit = tekstus[i];
+                    if ((ch == 0) && ((Char.IsUpper(lit)) || (Char.IsDigit(lit))))
+                    {
+                        MessageBox.Show("poza zakresem");
+                        blad = true;
+                        break;
+                    }
+                    else if ((ch == 1) && ((Char.IsLower(lit)) || (Char.IsDigit(lit))))
+                    {
+                        MessageBox.Show("poza zakresem");
+                        blad = true;
+                        break;
+                    }
+                    else if ((ch == 2) && (Char.IsLetter(lit)))
+                    {
+                        MessageBox.Show("poza zakresem");
+                        blad = true;
+                        break;
+                    }
+                    lit = (Char)(Convert.ToUInt16(lit) - kodus);
+                    if ((ch == 2) && (lit < 48))
+                    {
+                        lit = (Char)(Convert.ToUInt16(lit) + 10);
+                    }
+                    else if ((ch == 2) && (lit > 57))
+                    {
+                        lit = (Char)(Convert.ToUInt16(lit) - 10);
+                    }
+                    if ((ch == 0) && (kodus < 0))
+                    {
+                        lit = (Char)(Convert.ToUInt16(lit) - 26);
+                    }
+                    tekstus2 = tekstus2 + lit;
+                }
+            }
+            if (blad == false)
+            {
+                MessageBox.Show(tekstus2);
+            }
+            tekstus2 = "";
+        }
+    }
+}
